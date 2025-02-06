3cd73f2c6f2797b520717b54bbf1984d59600231
Elton Tay <57287843+eltontay@users.noreply.github.com>
2025-02-06 15:08:44 +0000

    First commit

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -3,18 +3,18 @@ export const CHAIN_CONFIG = {
     id: 11155111, // Sepolia
     name: "Ethereum Sepolia",
     domain: 0,
-    tokenMessenger: "0x1a9695e9dbdb443f0b8bb7626a7bf4421c22e594",
-    messageTransmitter: "0x7865c6E87B9F70255377e024ace6630C1Eaa37F9",
+    tokenMessenger: "0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa",
+    messageTransmitter: "0xe737e5cebeeba77efe34d4aa090756590b1ce275",
     usdc: "0x1c7d4b196cb0c7b01d743fbc6116a902379c7238",
   },
-  avalanche: { 
+  avalanche: {
     id: 43113, // Fuji
     name: "Avalanche Fuji",
     domain: 1,
-    tokenMessenger: "0xeb08f243e5d3fcff26a9e38ae5520a669f4019d0",
-    messageTransmitter: "0xa9fb1b3009dcb79e2fe346c16a604b8fa8ae0a79",
+    tokenMessenger: "0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa",
+    messageTransmitter: "0xe737e5cebeeba77efe34d4aa090756590b1ce275",
     usdc: "0x5425890298aed601595a70ab815c96711a31bc65",
-  }
+  },
 };
 
 export const CIRCLE_API_URL = "https://api.circle.com/v2";
